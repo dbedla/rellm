6d51e116c31701ad9ab543f9b4de9dd1962d35f3
Dawid <dbedla@noreply.codeberg.org>
2026-06-15 15:50:37 +0200

    base weather agent

---
+++ examples/ask_like_pro_agent/agent.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"rellm/pkg/rellm"
+)
+
+const (
+	askLikeProAgentSysPrompt = `You are a helpful assistant with deep weather knowledge.`
+)
+
+func buildLAskLikeProAgent(workspace string) *rellm.Agent {
+
+	agentName := "AskLikeProAgent"
+
+	or := rellm.NewCustomResponseEndpoint("http://127.0.0.1", "1234", "/v1/responses", nil)
+	ep := rellm.NewEndpointBuilder().
+		WithResponseApiEndpoint(or).
+		WithModel(rellm.Model_LMS_Google_Gemma_4_26B_A4B).
+		Build()
+
+	baseAgent := rellm.NewAgentBuilder().
+		WithEndpoint(ep).
+		WithAgentName(agentName).
+		WithWorkspaceDir(workspace).
+		WithMaxToolsIterationWithoutReturnMessage(20).
+		WithContinueConversation(false).
+		WithSystemMessage(askLikeProAgentSysPrompt).
+		WithToolset(&WeatherToolset{}).
+		Build()
+
+	return baseAgent
+}
---
+++ examples/ask_like_pro_agent/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"rellm/pkg/utils"
+
+	"github.com/fatih/color"
+)
+
+func main() {
+
+	agentLogDir, err := utils.CreateDirInSysTmp("agent-log")
+	if err != nil {
+		panic(err)
+	}
+
+	color.Red("Agent log dir: %s", agentLogDir)
+
+	agent := buildLAskLikeProAgent(agentLogDir)
+
+	defer func() {
+		err := agent.StoreConversation()
+		if err != nil {
+			color.Red("unable to store conversation: %s", err)
+		}
+	}()
+
+	for {
+		msg := utils.ReadConsoleInput()
+		if msg == "EXIT" {
+			return
+		}
+		llmResp := agent.Ask(msg)
+		color.Blue(llmResp)
+	}
+
+}
