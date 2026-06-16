fee9fd79d25dc0bd986d73ec13ee94c030ee3322
Dawid <dbedla@noreply.codeberg.org>
2026-06-16 16:55:34 +0200

    adapt custom endpoint to work with openrouter, crate, new or agent

---
+++ examples/openrouter_agent/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"rellm/pkg/utils"
+
+	"github.com/fatih/color"
+)
+
+func main() {
+	agentLogDir, err := utils.CreateDirInSysTmp("agent-log")
+	if err != nil {
+		panic(err)
+	}
+
+	color.Red("Agent log dir: %s", agentLogDir)
+
+	agent, err := buildLBaseAgent(agentLogDir)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		err := agent.StoreConversation()
+		if err != nil {
+			color.Red("unable to store conversation: %s", err)
+		}
+	}()
+
+	for {
+		msg := utils.ReadConsoleInput()
+		if msg == "EXIT" {
+			return
+		}
+		llmResp, err := agent.Ask(msg)
+		if err != nil {
+			color.Red("unable to ask question: %s", err.Error())
+			continue
+		}
+		color.Blue(llmResp)
+	}
+
+}
--- pkg/rellm/available_endpoints.go
+++ pkg/rellm/available_endpoints.go
@@ -2,38 +2,7 @@ package rellm
 
 import (
 	"net/http"
-	"os"
-
-	"github.com/joho/godotenv"
 )
-
-type OpenRouterEndpoint struct {
-}
-
-func (e *OpenRouterEndpoint) GetUrl() string {
-	return "https://openrouter.ai/api/v1/responses"
-}
-
-func (e *OpenRouterEndpoint) getApiKey() string {
-	err := godotenv.Load()
-	if err != nil {
-		panic("cannot load .env: " + err.Error())
-	}
-
-	apiKey := os.Getenv("OPENROUTER_API_KEY")
-	if apiKey == "" {
-		panic("missing apikey for OPENROUTER_API_KEY")
-	}
-
-	return apiKey
-}
-
-func (e *OpenRouterEndpoint) GetHttpHeader() http.Header {
-	header := make(http.Header)
-	header.Set("Content-Type", "application/json")
-	header.Set("Authorization", "Bearer "+e.getApiKey())
-	return header
-}
 
 type CustomResponseEndpoint struct {
 	baseUrl             string
@@ -52,7 +21,12 @@ func NewCustomResponseEndpoint(baseUrl, port, responseApiEndpoint string, httpHe
 }
 
 func (e *CustomResponseEndpoint) GetUrl() string {
-	return e.baseUrl + ":" + e.port + e.responseApiEndpoint
+	port := ""
+	if e.port != "" {
+		port = ":" + e.port
+	}
+
+	return e.baseUrl + port + e.responseApiEndpoint
 }
 
 func (e *CustomResponseEndpoint) GetHttpHeader() http.Header {
