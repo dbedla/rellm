546509e98b05213dd9952b2383814d20fe38b1f9
Dawid <dbedla@noreply.codeberg.org>
2026-08-25 15:46:34 +0200

    rm assert notes

--- pkg/rellm/agent_tools_test.go
+++ pkg/rellm/agent_tools_test.go
@@ -526,7 +526,7 @@ func TestAgentLMSTooManyFunctionCall(t *testing.T) {
 	ctx := context.Background()
 	_, err = agent.Execute(ctx, prompt)
 	assert.Error(t, err, "expected error when tool iteration budget is exceeded")
-	assert.True(t, errors.Is(err, rellm.ErrMaxAgentStepsReached), "error should wrap ErrMaxToolIterationsReached")
+	assert.True(t, errors.Is(err, rellm.ErrMaxAgentStepsReached))
 }
 
 func TestFuncResultToFunctionCallRespSerializesOutputAsString(t *testing.T) {
